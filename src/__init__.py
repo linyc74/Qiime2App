7a83d8ed9c56d5e651c31c3c8245f2e340b25c89
linyc74 <ylin@nycu.edu.tw>
2024-04-05 23:38:18 +0800

    version 1.2.2

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from .io import IO
 from .controller import Controller
 
 
-VERSION = 'v1.2.2-beta'
+VERSION = 'v1.2.2'
 STARTING_MESSAGE = f'''\
 Qiime2App {VERSION}
 College of Dentistry, National Yang Ming Chiao Tung University (NYCU), Taiwan
