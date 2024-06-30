59205589897333f7e520bcf0374a9b2ba1769b61
linyc74 <ylin@nycu.edu.tw>
2024-06-30 13:39:18 +0800

    basic mode and advanced mode for UI

--- src/view.py
+++ src/view.py
@@ -32,44 +32,78 @@ DEFAULT_KEY_VALUES = {
     'invert-colors': False,
     'publication-figure': False,
     'skip-differential-abundance': False,
-    'skip-picrust2': False,
+    'skip-picrust2': True,
     'threads': ['1', '4', '8'],
 }
-SSH_KEYS = [
-    'User',
-    'Host',
-    'Port',
-    'Qiime2 Pipeline',
-]
-QIIME2_KEYS = [
-    'sample-sheet',
-    'fq-dir',
-    'fq1-suffix',
-    'fq2-suffix',
-    'nb-classifier-qza',
-    'outdir',
-    'paired-end-mode',
-    'skip-otu',
-    'classifier-reads-per-batch',
-    'alpha-metrics',
-    'clip-r1-5-prime',
-    'clip-r2-5-prime',
-    'max-expected-error-bases',
-    'heatmap-read-fraction',
-    'n-taxa-barplot',
-    'beta-diversity-feature-level',
-    'colormap',
-    'invert-colors',
-    'publication-figure',
-    'skip-differential-abundance',
-    'skip-picrust2',
-    'threads',
-]
 BUTTON_NAME_TO_LABEL = {
+    'basic_mode': 'Basic Mode',
+    'advanced_mode': 'Advanced Mode',
     'load_parameters': 'Load Parameters',
     'save_parameters': 'Save Parameters',
     'submit': 'Submit',
 }
+
+
+class BasicMode:
+    SSH_KEYS = [
+        'User',
+        'Host',
+        'Port',
+        'Qiime2 Pipeline',
+    ]
+    QIIME2_KEYS = [
+        'sample-sheet',
+        'fq-dir',
+        'fq1-suffix',
+        'fq2-suffix',
+        'nb-classifier-qza',
+        'outdir',
+    ]
+    BUTTON_NAMES = [
+        'advanced_mode',
+        'load_parameters',
+        'save_parameters',
+        'submit',
+    ]
+
+
+class AdvancedMode:
+    SSH_KEYS = [
+        'User',
+        'Host',
+        'Port',
+        'Qiime2 Pipeline',
+    ]
+    QIIME2_KEYS = [
+        'sample-sheet',
+        'fq-dir',
+        'fq1-suffix',
+        'fq2-suffix',
+        'nb-classifier-qza',
+        'outdir',
+        'paired-end-mode',
+        'skip-otu',
+        'classifier-reads-per-batch',
+        'alpha-metrics',
+        'clip-r1-5-prime',
+        'clip-r2-5-prime',
+        'max-expected-error-bases',
+        'heatmap-read-fraction',
+        'n-taxa-barplot',
+        'beta-diversity-feature-level',
+        'colormap',
+        'invert-colors',
+        'publication-figure',
+        'skip-differential-abundance',
+        'skip-picrust2',
+        'threads',
+    ]
+    BUTTON_NAMES = [
+        'basic_mode',
+        'load_parameters',
+        'save_parameters',
+        'submit',
+    ]
 
 
 class View(QWidget):
@@ -78,16 +112,19 @@ class View(QWidget):
     ICON_PNG = f'{os.getcwd()}/icon/logo.ico'
     WIDTH, HEIGHT = 800, 1000
 
-    question_layout: QVBoxLayout
+    # UI items
     label_combo_pairs: List[Tuple[QLabel, QComboBox]]
-
-    button_layout: QHBoxLayout
     buttons: Dict[str, QPushButton]
 
+    # UI layouts
+    question_layout: QVBoxLayout
+    button_layout: QHBoxLayout
     scroll_area: QScrollArea
     scroll_contents: QWidget
-
     main_layout: QVBoxLayout
+
+    mode: Union[BasicMode, AdvancedMode]
+    shown_label_combo_pairs: List[Tuple[QLabel, QComboBox]]
 
     def __init__(self):
         super().__init__()
@@ -97,15 +134,21 @@ class View(QWidget):
 
         self.__init_label_combo_pairs()
         self.__init_buttons()
+
+        self.__init_question_layout()
+        self.__init_button_layout()
         self.__init_scroll_area_and_contents()
         self.__init_main_layout()
-        self.__init_methods()
+
+        self.__init_ui_methods()
+
+        self.show_basic_mode()
 
     def __init_label_combo_pairs(self):
-        self.question_layout = QVBoxLayout()
         self.label_combo_pairs = []
         for key, values in DEFAULT_KEY_VALUES.items():
             label = QLabel(f'{key}:', self)
+
             if type(values) is bool:
                 combo = QCheckBox(self)
                 combo.setChecked(values)
@@ -113,20 +156,31 @@ class View(QWidget):
                 combo = QComboBox(self)
                 combo.addItems(values)
                 combo.setEditable(True)
+
+            label.hide()
+            combo.hide()
+
             self.label_combo_pairs.append((label, combo))
-            self.question_layout.addWidget(label)
-            self.question_layout.addWidget(combo)
 
     def __init_buttons(self):
-        self.button_layout = QHBoxLayout()
-        self.button_layout.addStretch(1)
-        self.question_layout.addLayout(self.button_layout)
-
         self.buttons = dict()
         for name, label in BUTTON_NAME_TO_LABEL.items():
             button = QPushButton(label, self)
-            self.button_layout.addWidget(button)
             self.buttons[name] = button
+            button.hide()
+
+    def __init_question_layout(self):
+        self.question_layout = QVBoxLayout()
+        for label, combo in self.label_combo_pairs:
+            self.question_layout.addWidget(label)
+            self.question_layout.addWidget(combo)
+
+    def __init_button_layout(self):
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addStretch(1)
+        self.question_layout.addLayout(self.button_layout)
+        for button in self.buttons.values():
+            self.button_layout.addWidget(button)
 
     def __init_scroll_area_and_contents(self):
         self.scroll_area = QScrollArea(self)
@@ -140,7 +194,7 @@ class View(QWidget):
         self.main_layout.addWidget(self.scroll_area)  # add scroll_area to the main_layout
         self.setLayout(self.main_layout)
 
-    def __init_methods(self):
+    def __init_ui_methods(self):
         self.message_box_info = MessageBoxInfo(self)
         self.message_box_error = MessageBoxError(self)
         self.message_box_yes_no = MessageBoxYesNo(self)
@@ -148,18 +202,45 @@ class View(QWidget):
         self.file_dialog_save = FileDialogSave(self)
         self.password_dialog = PasswordDialog(self)
 
+    def show_basic_mode(self):
+        self.mode = BasicMode()
+        self.__show_mode()
+
+    def show_advanced_mode(self):
+        self.mode = AdvancedMode()
+        self.__show_mode()
+
+    def __show_mode(self):
+        self.shown_label_combo_pairs = []
+
+        for label, combo in self.label_combo_pairs:
+            key = label.text()[:-1]
+            if key in self.mode.QIIME2_KEYS + self.mode.SSH_KEYS:
+                label.show()
+                combo.show()
+                self.shown_label_combo_pairs.append((label, combo))
+            else:
+                label.hide()
+                combo.hide()
+
+        for name, button in self.buttons.items():
+            if name in self.mode.BUTTON_NAMES:
+                button.show()
+            else:
+                button.hide()
+
     def get_key_values(self) -> Dict[str, Union[str, bool]]:
-        return self.__get_key_values(keys=SSH_KEYS + QIIME2_KEYS)
+        return self.__get_key_values(keys=self.mode.SSH_KEYS + self.mode.QIIME2_KEYS)
 
     def get_ssh_key_values(self) -> Dict[str, Union[str, bool]]:
-        return self.__get_key_values(keys=SSH_KEYS)
+        return self.__get_key_values(keys=self.mode.SSH_KEYS)
 
     def get_qiime2_key_values(self) -> Dict[str, Union[str, bool]]:
-        return self.__get_key_values(keys=QIIME2_KEYS)
+        return self.__get_key_values(keys=self.mode.QIIME2_KEYS)
 
     def __get_key_values(self, keys: List[str]) -> Dict[str, str]:
         ret = {}
-        for label, item in self.label_combo_pairs:
+        for label, item in self.shown_label_combo_pairs:
             k = label.text()[:-1]
             if k not in keys:
                 continue
@@ -172,12 +253,12 @@ class View(QWidget):
     def set_parameters(self, parameters: Dict[str, Union[str, bool]]):
         # Reset flags to False because
         #   when a flag is not present in parameters, it should be False
-        for _, combo in self.label_combo_pairs:
+        for _, combo in self.shown_label_combo_pairs:
             if type(combo) is QCheckBox:
                 combo.setChecked(False)
 
         for key, val in parameters.items():
-            for label, combo in self.label_combo_pairs:
+            for label, combo in self.shown_label_combo_pairs:
                 if label.text()[:-1] == key:
                     if type(combo) is QComboBox:
                         combo.setCurrentText(val)
