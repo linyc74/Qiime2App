39055ec602fdc192ef4ab42f825e163a693803b1
linyc74 <ylin@nycu.edu.tw>
2024-07-01 14:31:15 +0800

    version 1.4.0

--- src/view.py
+++ src/view.py
@@ -10,7 +10,7 @@ EDIT_KEY_TO_VALUES = {
     'User': [''],
     'Host': ['255.255.255.255'],
     'Port': ['22'],
-    'Qiime2 Pipeline': ['qiime2_pipeline-2.3.0'],
+    'Qiime2 Pipeline': ['qiime2_pipeline-2.5.0'],
 
     'sample-sheet': ['sample-sheet.csv'],
     'fq-dir': ['data'],
