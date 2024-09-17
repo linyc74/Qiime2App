4c0ceee62bd4c3cbf3c2d8c58f7fbf00716263b8
linyc74 <ylin@nycu.edu.tw>
2024-09-17 16:32:46 +0800

    add options for vsearch feature classifier

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from .io import IO
 from .controller import Controller
 
 
-VERSION = 'v1.5.1'
+VERSION = 'v1.6.0-beta'
 STARTING_MESSAGE = f'''\
 Qiime2App {VERSION}
 College of Dentistry, National Yang Ming Chiao Tung University (NYCU), Taiwan
